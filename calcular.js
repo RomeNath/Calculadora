c2ec445c4bf7959ef8639a3b2c1f53c4d6b5ca9a
RomeNath <nathromero.95@gmail.com>
2019-05-31 14:30:54 -0600

    first commit

---
+++ calcular.js
@@ -0,0 +1,16 @@
+<script type="text/javascript"> 
+function calcpropina (cuenta, propina,personas) {
+	var cuenta= document.getElementById('cuenta');
+	var personas = document.getElementById('personas');
+	
+	var value= document.getElementById('value1') =0.1;
+	var value= document.getElementById('value2') =0.15;
+	var value= document.getElementById('value3') =0.20;
+	var value= document.getElementById('value4') =0.25; 
+	
+	var 'calcular' = ('cuenta' + 'value1'); 
+	alert( 'calcular' ); 
+}
+
+	
+</script> 
